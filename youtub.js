4e8d6beffbd6e78e97d34b1ae1f211c907fdf3a4
kapil kumar <kapilkumar122414@gmail.com>
2025-01-24 13:10:00 +0530

    add new file

---
+++ youtub.js
@@ -0,0 +1,15 @@
+function data(){
+    
+    document.getElementById("student").innerText=document.getElementById("student_name").value;
+    document.getElementById("schoolst").innerText =document.getElementById("school").value;
+    document.getElementById("Rollst").innerText =document.getElementById("Roll").value;
+    document.getElementById("contectst").innerText=document.getElementById("contect").value;
+    document.getElementById("addressst").innerText=document.getElementById("Address").value;
+    document.getElementById("classst").innerText=document.getElementById("class").value;
+    document.getElementById("Dobst").innerText=document.getElementById("DOB").value;
+    
+}
+
+function printid(){
+    window.print();
+};
